43eab988c7ae2ebb6a1eb5e1c8ec811d50852abc
AkaKwak <christophe.alaterre@gmail.com>
2024-11-25 14:17:09 +0100

    Add controller / Add model / Add view / Fix Rubocop asking better syntax in schema

---
+++ db/migrate/20241125131316_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[8.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+      t.integer :stock
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
