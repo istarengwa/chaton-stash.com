7a96eb6f3c367d2def0760575e69bec3b1d84269
hellijah <thierry.corlieto@gmail.com>
2024-11-25 16:49:43 +0100

    update table order

--- db/migrate/20241125152539_create_orders.rb
+++ db/migrate/20241125152539_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[8.0]
   def change
     create_table :orders do |t|
       t.references :user, null: false, foreign_key: true
-      # t.string :status
+      t.string :status
       t.decimal :total_price
 
       t.timestamps
