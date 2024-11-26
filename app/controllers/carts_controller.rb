6c86ba2b46f3f8de54d20966f7944e42e0b90c02
hellijah <thierry.corlieto@gmail.com>
2024-11-26 19:42:10 +0100

    cart controller + view show + comment db

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,20 +3,22 @@ class CartsController < ApplicationController
 
   def show
     @cart_items = @cart.cart_items.includes(:item)
+    @total_price = @cart_items.sum { |ci| ci.item.price }
   end
 
   def update
-    cart_item = @cart.cart_items.find(params[:id])
-    if cart_item.update(quantity: params[:quantity])
-      redirect_to cart_path, notice: 'Quantité mise à jour.'
+    cart_item = @cart.cart_items.find_by(id: params[:cart_item_id])
+
+    if cart_item&.destroy # &. safe navigation operator si cart_item est nill alors il retourne nil
+      redirect_to cart_path, notice: 'Item retiré du panier.'
     else
-      redirect_to cart_path, alert: 'Erreur lors de la mise à jour.'
+      redirect_to cart_path, alert: 'Erreur lors de la mise à jour du panier.'
     end
   end
 
   def destroy
     @cart.cart_items.destroy_all
-    redirect_to cart_path, notice: 'Panier vidé.'
+    redirect_to cart_path, notice: 'Panier vidé.' # redirect à modifier quand panier transféré à commande
   end
 
   private
