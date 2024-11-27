b0e3b91ea51c56b9d0cfd8501e5540019c8f6b22
RAMASSAMY Luc <luko974@gmail.com>
2024-11-27 18:33:47 +0100

    commented mailer relative for fixing prod

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :email_address, presence: true, uniqueness: true
   normalizes :email_address, with: ->(e) { e.strip.downcase }
   after_create :welcome_send
-  def welcome_send
-    UserMailer.welcome_email(self).deliver_now
-  end
+  #def welcome_send
+  #  UserMailer.welcome_email(self).deliver_now
+  #end
 end
